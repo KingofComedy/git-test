94d7902eee45623bb90cd50c84078c2a8b6ab94f
lee <lee@163.com>
2022-01-11 22:03:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Li Runle
+ * @date 2022/1/11 - 21:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
